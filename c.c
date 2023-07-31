e5915e15200749ff945892b7f7a25e1ae6ac1554
jothikap-cs21 <jothikap.cs21@kascsathy.ac.in>
2023-07-31 09:40:42 +0000

    fourth commit

---
+++ c.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+struct node
+{
+        int a,b,c,d;
+        };
+int main()
+{
+    struct node x;
+x.a=10;
+x.b=20;
+x.c=20;
+x.d=30;
+printf("%d",x.a);
+printf("%d",x.b);
+printf("%d",x.c);
+printf("%d",x.d);
+return 0;
+}
+
+
+
+
+
+
+
